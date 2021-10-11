af65710f11e34a95b50be9f37d624ebde84fd3f5
Felipe Dias <felipeferreiradias1@gmail.com>
2021-10-11 17:27:27 -0300

    add trybeWarts project

---
+++ script.js
@@ -0,0 +1,48 @@
+function loginAuth() {
+  const submitButton = document.querySelector('#login-button');
+  submitButton.addEventListener('click', (event) => {
+    const loginButton = document.querySelector('#login').value;
+    const passwordButton = document.querySelector('#password').value;
+    event.preventDefault();
+
+    if (loginButton === 'tryber@teste.com' && passwordButton === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
+
+loginAuth();
+
+function enableButton() {
+  const agreement = document.querySelector('#agreement');
+  const button = document.querySelector('#submit-btn');
+
+  agreement.addEventListener('click', () => {
+    if (button.disabled === true) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  });
+}
+
+enableButton();
+
+function textareaCounter() {
+  const text = document.getElementById('textarea');
+  const counter = document.getElementById('counter');
+
+  text.addEventListener('keyup', () => {
+    const characterLength = text.value.length;
+    counter.setAttribute('value', (500 - characterLength));
+    counter.innerText = `${(counter.getAttribute('value'))}${'/500'}`;
+  });
+}
+
+textareaCounter();
+
+function replaceFields () {
+  const text = document.getElementById('textarea');
+}
